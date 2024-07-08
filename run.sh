e495fd2ae0e6c2080d1d2f5a17a82ee4792236e2
Yao Lu <lu.yao@ucl.ac.uk>
2024-07-08 13:39:24 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,3 +2,5 @@
 
 python3 main.py --model gpt2 --num_random_draw 160 --context_shot_size 1 --corpus_size 64 --optimization_mode random_vocab --seed 1
 
+python3 main.py --model gpt2-large --num_random_draw 160 --context_shot_size 1 --corpus_size 64 --optimization_mode random_wo_context --seed 1
+
